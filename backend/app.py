064a829e39f1e8bd0a29a2dafe3eb02741d3995e
Sungmu <smc6189@gmail.com>
2025-06-03 11:10:48 -0700

    change for test docker

--- backend/app.py
+++ backend/app.py
@@ -46,5 +46,5 @@ def predict():
             os.remove(save_path)
 
 if __name__ == '__main__':
-    # For local development: Start the Flask server
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000)) 
+    app.run(host='0.0.0.0', port=port, debug=False)
